614b1b9d6d1b237fd1d37b0a6bc9ff70285e9a3b
mochi5o <1610.baumkuchen.124@gmail.com>
2021-04-18 15:10:16 +0900

    Delete github_api.rb

--- github_api.rb
+++
@@ -1,9 +0,0 @@
-require_relative './.env.rb'
-require 'octokit'
-@name = 'mochi5o'
-@token = $ACCESS_TOKEN
-
-client = Octokit::Client.new(:access_token => @token)
-
-events = client.repository_issue_events(:repo => 'TIL', :user => @name)
-p events[0]
